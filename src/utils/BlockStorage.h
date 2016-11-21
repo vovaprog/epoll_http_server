3b1f4500803de3d52f4043dd1772475b121e4612
Vladimir Silaev <vovaprog@yandex.ru>
2016-11-21 11:57:48 +0300

    block storage test

--- src/utils/BlockStorage.h
+++ src/utils/BlockStorage.h
@@ -8,7 +8,7 @@ template<class T>
 class BlockStorage
 {
 public:
-
+ 
     BlockStorage() = default;
 
     BlockStorage(const BlockStorage &bs) = delete;
@@ -22,6 +22,12 @@ public:
         destroy();
     }
 
+
+    struct ServiceData {
+        int index = -1;
+        bool used = false;
+    };
+ 
 
     int init(int maxBlocks, int blockSize)
     {
@@ -38,7 +44,6 @@ public:
         }
 
         empty.clear();
-        used.clear();
 
         return 0;
     }
@@ -62,11 +67,83 @@ public:
         }
 
         empty.clear();
-        used.clear();
 
         nextBlock = 0;
         maxBlocks = 0;
         blockSize = 0;
+    }
+
+
+    T* allocate()
+    {
+        if(empty.size() <= 0)
+        {
+            if(allocateBlock() != 0)
+            {
+                return nullptr;
+            }
+            if(empty.size() <= 0)
+            {
+                return nullptr;
+            }
+        }
+
+        int index = empty.back();
+        empty.pop_back();
+
+        return get(index);
+    }
+
+
+    int free(T *item)
+    {
+        empty.push_back(item->bsData.index);
+        return 0;
+    }
+
+
+    struct StorageInfo
+    {
+        int maxBlocks = 0;
+        int blockSize = 0;
+        int allocatedBlocks = 0;
+        int emptyItems = 0;
+    };
+
+    int getStorageInfo(StorageInfo &si) const
+    {
+        si.maxBlocks = maxBlocks;
+        si.blockSize = blockSize;
+        si.allocatedBlocks = nextBlock;
+        si.emptyItems = static_cast<int>(empty.size());
+
+        return 0;
+    }
+
+
+protected:
+
+    int allocateBlock()
+    {
+        if(nextBlock >= maxBlocks)
+        {
+            return -1;
+        }
+        data[nextBlock] = new T[blockSize];
+
+        int index = nextBlock * blockSize;
+
+        for(int i = 0; i < blockSize; ++i)
+        {
+            data[nextBlock][i].bsData.index = index;
+            data[nextBlock][i].bsData.used = false;
+            empty.push_back(index);
+            ++index;
+        }
+
+        ++nextBlock;
+
+        return 0;
     }
 
 
@@ -87,88 +164,7 @@ public:
         return &data[blockIndex][itemIndex];
     }
 
-
-    T* allocate()
-    {
-        if(empty.size() <= 0)
-        {
-            if(allocateBlock() != 0)
-            {
-                return nullptr;
-            }
-            if(empty.size() <= 0)
-            {
-                return nullptr;
-            }
-        }
-
-        int index = empty.back();
-        empty.pop_back();
-        used.insert(index);
-
-        return get(index);
-    }
-
-
-    int free(int index)
-    {
-        empty.push_back(index);
-        used.erase(index);
-        return 0;
-    }
-
-
-    const std::set<int>& usedIndexes() const
-    {
-        return used;
-    }
-
-
-    struct StorageInfo
-    {
-        int maxBlocks = 0;
-        int blockSize = 0;
-        int allocatedBlocks = 0;
-        int usedItems = 0;
-        int emptyItems = 0;
-    };
-
-    int getStorageInfo(StorageInfo &si) const
-    {
-        si.maxBlocks = maxBlocks;
-        si.blockSize = blockSize;
-        si.allocatedBlocks = nextBlock;
-        si.usedItems = static_cast<int>(used.size());
-        si.emptyItems = static_cast<int>(empty.size());
-
-        return 0;
-    }
-
-protected:
-
-    int allocateBlock()
-    {
-        if(nextBlock >= maxBlocks)
-        {
-            return -1;
-        }
-        data[nextBlock] = new T[blockSize];
-
-        int index = nextBlock * blockSize;
-
-        for(int i = 0; i < blockSize; ++i)
-        {
-            data[nextBlock][i].index = index;
-            empty.push_back(index);
-            ++index;
-        }
-
-        ++nextBlock;
-
-        return 0;
-    }
-
-
+   
 protected:
 
     T **data = nullptr;
@@ -177,7 +173,6 @@ protected:
     int nextBlock = 0;
 
     std::vector<int> empty;
-    std::set<int> used;
 };
 
 #endif
--- tests/TestBlockStorage.cpp
+++ tests/TestBlockStorage.cpp
@@ -1,24 +1,111 @@
 #include <BlockStorage.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <chrono>
+
 struct Data {
     int index;
     char buf[200];
+    BlockStorage<Data>::ServiceData bsData;
 };
 
+const int totalItems = 10000000;
+const int blockSize = 100;
+
 BlockStorage<Data> storage;
+std::vector<Data*> pointers;
+
+
+inline long long int getMilliseconds()
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+}
+
+
+Data* blockStorageAllocate(int size)
+{
+    return storage.allocate();
+}
+
+void blockStorageFree(Data *data)
+{
+    storage.free(data);
+}
+
+template<typename Allocate, typename Free>
+void testStorage(Allocate allocFun, Free freeFun)
+{
+    for(int i = 0; i < totalItems; ++i)
+    {
+        Data *ptr = (Data*)allocFun(sizeof(Data));
+        if(ptr == nullptr)
+        {
+            printf("allocation failed!\n");
+            exit(-1);
+        }
+        pointers.push_back(ptr);
+    }
+
+    const int numberOfIters = 1;
+    const int testBlock = 10;
+
+    for(int q = 0; q < numberOfIters; ++q)
+    {
+        for(int start = 0; start < testBlock; ++start)
+        {
+            for(int ind = start; ind < totalItems; ind += testBlock)
+            {        
+                freeFun(pointers[ind]);
+            }
+            for(int ind = start; ind < totalItems; ind += testBlock)
+            {      
+                Data *ptr = (Data*)allocFun(sizeof(Data));
+                if(ptr == nullptr)
+                {
+                    printf("allocation failed!\n");
+                    exit(-1);
+                }                
+                pointers[ind] = ptr;
+            }
+        }
+    }
+
+    for(Data *p : pointers)
+    {
+        freeFun(p);
+    }
+}
+
 
 int main()
 {
-    const int totalItems = 1000000;
-    const int blockSize = 100;
     storage.init(totalItems / blockSize, blockSize);
-    std::vector<Data*> pointers;
     pointers.reserve(totalItems);
 
-    for(int i = 0; i < totalItems; ++i)
-    {
-        pointers.push_back(storage.allocate());
+    long long int millis;
+
+     {
+        millis = getMilliseconds();
+
+        testStorage(blockStorageAllocate, blockStorageFree);
+
+        millis = getMilliseconds() - millis;
+        printf("block storage: %lld\n", millis);
     }
+  
+    pointers.erase(pointers.begin(), pointers.end());
+ 
+    {
+        millis = getMilliseconds();
+
+        testStorage(malloc, free);
+
+        millis = getMilliseconds() - millis;
+        printf("malloc: %lld\n", millis);
+   }
 
     return 0;
 }
